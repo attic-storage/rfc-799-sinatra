c8837880884bea459df80699ac871434e4cdebf5
Romain Lespinasse <romain.lespinasse@gmail.com>
2012-02-21 21:36:12 +0100

    maya EOW implementation into a sinatra app

---
+++ README.md
@@ -0,0 +1,5 @@
+# SINATRA 'End Of World' BOOTSTRAP
+
+it's a 'END OF THE WORLD' bootstrap for [sinatra](http://www.sinatrarb.com/) application.
+
+_use http status 799 define by_ [7XX-rfc project](https://github.com/joho/7XX-rfc)
---
+++ apps.rb
@@ -0,0 +1,26 @@
+require 'sinatra'
+
+helpers do
+  def eow()
+    pass if Time.now != time_for('Dec 21, 2012')
+    status 799
+    headers \
+      "Allow"   => "BREW, POST, GET, PROPFIND, WHEN",
+      "Refresh" => "Refresh: 20; https://github.com/joho/7XX-rfc"
+    body "It's the 'END OF THE WORLD' day, thanks to mayas"
+  end
+end
+# check the 'end of the world' trigger for all routes
+get '*' do
+  eow()
+end
+
+# put your application here
+get '/' do
+  status 204
+end
+
+# if any routes matchs, return HTTP 404 status
+get '*' do
+  status 404
+end
